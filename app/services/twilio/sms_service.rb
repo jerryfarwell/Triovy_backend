284f0112e886f3c4505d003cd8f4c8e98d78cfc4
JERRY farwell <114482666+jerryfarwell@users.noreply.github.com>
2023-09-20 18:15:26 +0200

    setting twilio quote

--- app/services/twilio/sms_service.rb
+++ app/services/twilio/sms_service.rb
@@ -1,29 +1,19 @@
-# app/services/sms_service.rb
 
-require 'twilio-ruby'
+# # app/services/twilio/sms_service.rb
+# require 'twilio-ruby'
 
-class SmsService
-  def initialize(quote)
-    @quote = quote
-    @account_sid = Rails.application.credentials.dig(:twilio, :account_sid)
-    @auth_token = Rails.application.credentials.dig(:twilio, :auth_token)
-  end
+# class SmsService
+#     account_sid = Rails.application.credentials.dig(:twilio, :account_sid)
+#     auth_token = Rails.application.credentials.dig(:twilio, :auth_token)
 
-  def send_quote_sms
-    @client = Twilio::REST::Client.new(@account_sid, @auth_token)
-  
-    message_body = "New Quote:\n"
-    @quote.attributes.each do |attr, value|
-      message_body << "#{attr}: #{value}\n"
-    end
+#     @client = Twilio::REST::Client.new(account_sid, auth_token)
+    
+#     message = @client.messages.create(
+#       body: 'this is the message from my app',
+#       from: '+17242046854',
+#       to: '+33753226897'
+#     )
+    
+#     puts message.sid 
+# end
 
-    message = @client.messages.create(
-      body: message_body,
-      from: '+17242046854',
-      to: '+33753226897'
-    )
-
-    puts "Your quote has been sent "
-    puts message.sid
-  end
-end
